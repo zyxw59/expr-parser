839907bf84449d32de347202d9e409f47f8bb465
emily Crandall Fleischman <emilycf@mit.edu>
2023-01-26 21:24:21 -0500

    join method on Span

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,16 @@ pub struct Span {
     pub end: usize,
 }
 
+impl Span {
+    /// Creates a new span encompassing both input spans.
+    pub fn join(self, other: Self) -> Self {
+        Span {
+            start: self.start.min(other.start),
+            end: self.end.max(other.end),
+        }
+    }
+}
+
 impl fmt::Display for Span {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}..{}", self.start, self.end)
