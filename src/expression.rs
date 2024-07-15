734ccdb5b3bc37c8928a60c277e9c1b7c8652817
emily Crandall Fleischman <zyxw59@gmail.com>
2024-07-14 22:25:49 -0400

    add eq/partialeq to expression

--- src/expression.rs
+++ src/expression.rs
@@ -1,12 +1,12 @@
 use crate::Span;
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct Expression<B, U, T> {
     pub kind: ExpressionKind<B, U, T>,
     pub span: Span,
 }
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub enum ExpressionKind<B, U, T> {
     BinaryOperator(B),
     UnaryOperator(U),
