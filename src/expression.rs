f1f55d95d583f36f3f3ceafe79cbfa707e7e1a3b
emily Crandall Fleischman <zyxw59@gmail.com>
2024-07-22 17:34:58 -0400

    ignore span for expression equality

--- src/expression.rs
+++ src/expression.rs
@@ -1,9 +1,16 @@
 use crate::Span;
 
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+#[derive(Clone, Copy, Debug, Eq)]
 pub struct Expression<B, U, T> {
     pub kind: ExpressionKind<B, U, T>,
     pub span: Span,
+}
+
+/// Expression equality ignores the span
+impl<B: PartialEq, U: PartialEq, T: PartialEq> PartialEq for Expression<B, U, T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.kind == other.kind
+    }
 }
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
